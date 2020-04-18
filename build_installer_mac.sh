574ff154d4e71d60672a8581fd8beb0cf227f363
Tim Muehle <tim.muehle@hs-bochum.de>
2020-04-18 22:14:29 +0200

    build_installer_mac korrigiert

--- build_installer_mac.sh
+++ build_installer_mac.sh
@@ -1,65 +1,61 @@
 #!/bin/sh
 
-# Dieses Script erstellt mit Hilfe des jpackage Tools (OpenJDK14) aus einer ausführbaren JAR Datei
-# einen nativen Installer für Windows. Alle von dem Java Programm benötigten Komponenten (JRE, javaFX, ...)
+# Dieses Script erstellt mit Hilfe des jpackage Tools (OpenJDK14) aus einer ausfuehrbaren JAR Datei
+# einen nativen Installer fuer Windows. Alle von dem Java Programm benoetigten Komponenten (JRE, javaFX, ...)
 # werden dabei mit in den Installer eingebunden, sodass das Programm nach der Installation auf jedem Windows Betriebssystem
-# unabhängig von anderen Programmen oder Installationen lauffähig ist.
+# unabhaengig von anderen Programmen oder Installationen lauffaehig ist.
 
-# Es werden zwei Installer in den gängigen Formaten bereitgestellt (.exe und .msi)
+# Es werden zwei Installer fuer macOS in den gaengigen Formaten bereitgestellt (.dmg und .pkg)
 
-# ---- Benötigte Informationen in Variablen speichern ----------------------------------------------------------------
+# ---- Benoetigte Informationen in Variablen speichern ----------------------------------------------------------------
 
-# Mit den folgeneden Variablen können die Grundlegenden Daten für das Projekt eingestellt werden:
-NAME=Bitchanger
-DESCRIPTION="Rechner für beliebige Zahlensysteme"
-VERSION=0.0.1
-VENDOR=Entwicklungsprojekt_EB2020
+# Mit den folgeneden Variablen koennen die Grundlegenden Daten fuer das Projekt eingestellt werden:
+NAME="Bitchanger"
+DESCRIPTION="Rechner fuer beliebige Zahlensysteme"
+VERSION="0.0.1"
+VENDOR="Entwicklungsprojekt_EB2020"
 # set COPYRIGHT = ""
 # set LICENSE_FILE = ""
 
 # Einstellungen fuer jpackage:
-MAIN_JAR=bitchanger-$VERSION-jar-with-dependencies.jar
-INPUT=target
-OUT=installer
+MAIN_JAR="bitchanger-$VERSION-jar-with-dependencies.jar"
+INPUT="target"
+OUT="installer"
 # set ICON =
 
-# Weitere Befehle für jpackage:
-# Installationspfad bei der Installation auswählbar: --win-dir-chooser
-# Shortcut auf dem Desktop: --win-shortcut
-# In das Startmenü aufnehmen: --win-menu
-# Startmenügruppe aufwählen: --win-menu-group <menu-group-name>
-# Installation auf Benutzerbasis: --win-per-user-install
-# App Icon ändern: --icon <path/to/icon.ico>
+# Weitere Befehle fuer jpackage:
+# Installationspfad bei der Installation auswaehlbar: --win-dir-chooser
+# Name, der in der Menueleiste angezeigt wird: --mac-package-name <name>
+# Request that the bundle be signed: --mac-sign
+# App Icon aendern: --icon <path/to/icon.ico>
 # Linzenz Datei: --license-file <file>
+
 
 # ---- Installer erzeugen ----------------------------------------------------------------------------------------------
 
-echo 
-echo Installer für macOS werden erzeugt.
-echo 
+echo ""
+echo "Installer fuer macOS werden erzeugt."
+echo ""
 
 for TYPE in "dmg" "pkg"
 do
-	echo ${NAME-$VERSION}.${TYPE} wird erstellt
-	echo 
+	echo "${NAME-$VERSION}.${TYPE} wird erstellt"
+	echo ""
 	
-	jpackage ^
-	--type dmg ^
-	--name $NAME ^
-	--description $DESCRIPTION ^
-	--vendor $VENDOR ^
-	--app-version $VERSION ^
-	--input $INPUT ^
-	--dest $OUT ^
-	--main-jar $MAIN_JAR ^
-	--mac-package-name $NAME ^
-	--mac-sign
+	/Library/Java/jdk-14.jdk/Contents/Home/bin/jpackage \
+	--type $TYPE \
+	--name "${NAME}" \
+	--description "${DESCRIPTION}" \
+	--vendor "${VENDOR}" \
+	--app-version $VERSION \
+	--input $INPUT \
+	--dest $OUT \
+	--main-jar "${MAIN_JAR}" \
+	--mac-package-name "${NAME}"
 
-	echo
-	echo
+	echo ""
+	echo ""
 done
 
-# ---- Auf Bestätigung von Benutzer warten -----------------------------------------------------------------------------
-read -p "Zum Abschließen eine beliebige Taste drücken ..." var
 
 
---
+++ build_mvn_and_installer_mac.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+# Dieses Script erstellt mit Hilfe des jpackage Tools (OpenJDK14) aus einer ausfuehrbaren JAR Datei
+# einen nativen Installer fuer Windows. Alle von dem Java Programm benoetigten Komponenten (JRE, javaFX, ...)
+# werden dabei mit in den Installer eingebunden, sodass das Programm nach der Installation auf jedem Windows Betriebssystem
+# unabhaengig von anderen Programmen oder Installationen lauffaehig ist.
+
+# Es werden zwei Installer fuer macOS in den gaengigen Formaten bereitgestellt (.dmg und .pkg)
+
+# ---- Benoetigte Informationen in Variablen speichern ----------------------------------------------------------------
+
+# Mit den folgeneden Variablen koennen die Grundlegenden Daten fuer das Projekt eingestellt werden:
+NAME="Bitchanger"
+DESCRIPTION="Rechner fuer beliebige Zahlensysteme"
+VERSION="0.0.1"
+VENDOR="Entwicklungsprojekt_EB2020"
+# set COPYRIGHT = ""
+# set LICENSE_FILE = ""
+
+# Einstellungen fuer jpackage:
+MAIN_JAR="bitchanger-$VERSION-jar-with-dependencies.jar"
+INPUT="target"
+OUT="installer"
+# set ICON =
+
+# Weitere Befehle fuer jpackage:
+# Installationspfad bei der Installation auswaehlbar: --win-dir-chooser
+# Name, der in der Menueleiste angezeigt wird: --mac-package-name <name>
+# Request that the bundle be signed: --mac-sign
+# App Icon aendern: --icon <path/to/icon.ico>
+# Linzenz Datei: --license-file <file>
+
+# ---- Maven build -----------------------------------------------------------------------------------------------------
+echo ""
+echo "Maven build durchfuehren"
+echo ""
+mvn clean install
+
+# ---- Installer erzeugen ----------------------------------------------------------------------------------------------
+
+echo ""
+echo "Installer fuer macOS werden erzeugt."
+echo ""
+
+for TYPE in "dmg" "pkg"
+do
+	echo "${NAME-$VERSION}.${TYPE} wird erstellt"
+	echo ""
+	
+	/Library/Java/jdk-14.jdk/Contents/Home/bin/jpackage \
+	--type $TYPE \
+	--name "${NAME}" \
+	--description "${DESCRIPTION}" \
+	--vendor "${VENDOR}" \
+	--app-version $VERSION \
+	--input $INPUT \
+	--dest $OUT \
+	--main-jar "${MAIN_JAR}" \
+	--mac-package-name "${NAME}"
+
+	echo ""
+	echo ""
+done
+
+
+
