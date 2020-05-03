f51cb9dad9e15f10af121d2efbc8c39809486d9b
Moritz Wolter <moritz.wolter@hs-bochum.de>
2020-05-03 10:04:32 +0200

    erstellung der Methode basisToDezString und Korrektur der Methode basisToDez
    -basisToDezString (wandelt die übergebene Zahl zur spezifischen Basis in der String-Darstellung in eine Zahl zur Basis 10 und gibt diese als in der String-Darstellung zurück)
    
    -Verwendete Variablen ins englische übersetzt

--- src/main/java/bitchanger/components/ConvertingNumbers.java
+++ src/main/java/bitchanger/components/ConvertingNumbers.java
@@ -7,10 +7,11 @@
 
 package bitchanger.components;
 
+import zahlenRechner.Zahl;
 
 /**
- * Diese Klasse stellt Methoden zum Umwandeln von Zahlen mit verschiedenen Zahlensystemen bereit.
  * This class contains methods to convert numbers into different numeral systems.
+ * 
  * @author Tim & Moritz
  *
  */
@@ -22,7 +23,7 @@ public class ConvertingNumbers {
 	/**
 	 * wandelt die übergebene Zahl zur spezifischen Basis in der String-Darstellung 
 	 * in eine Zahl zur Basis 10 und gibt diese als {@code double} zurück
-	 * @param basis	die spezifische Basis des übergebenen wertes
+	 * @param base	die spezifische Basis des übergebenen wertes
 	 * @param value	der Zahlenwert, der umgewandelt werden soll übergeben in der String-Darstellung
 	 * @return	Darstellung des Wertes im Zehnersystem als {@code double} Zahl
 	 * @throws 	NullPointerException	wenn der Parameter {@code wert} null ist.
@@ -30,7 +31,7 @@ public class ConvertingNumbers {
 	 * @throws 	UnsupportedOperationException	wenn das erste Zeichen des Parameters {@code zahl} ein '-' ist, 
 	 * 											da negative Zahlen hier nicht erlaubt sind
 	 */
-	public static double basisToDez(int basis, String value) throws NullPointerException, NumberFormatException, UnsupportedOperationException {
+	public static double basisToDez(int base, String value) throws NullPointerException, NumberFormatException, UnsupportedOperationException {
 		// Übergebene Zahl in ganzen Anteil und Nachkommastellen trennen
 		value = value.toUpperCase();
 		String ganzerWert = Zahl.separiereGanzenAnteil(value);
@@ -38,13 +39,63 @@ public class ConvertingNumbers {
 		
 		// Strings die ganzen und Nachkommateil zu der übergebenen Basis 
 		// repräsentieren in double Zahlen zur Basis 10 umwandeln
-		double ganzDez = basisToDezGanz(basis, ganzerWert);
-		double nachDez = basisToDezNachKomma(basis, nachKomma);
+		double ganzDez = basisToDezGanz(base, ganzerWert);
+		double nachDez = basisToDezNachKomma(base, nachKomma);
 		
 		// Vor- und Nachkommateil addieren und Ergebnis zurückgeben
 		return ganzDez + nachDez;
 	}
 	
+	/**
+	 * wandelt die übergebene Zahl zur spezifischen Basis in der String-Darstellung 
+	 * in eine Zahl zur Basis 10 und gibt diese als in der String-Darstellung zurück
+	 * @param base	die spezifische Basis des übergebenen wertes
+	 * @param value	der Zahlenwert, der umgewandelt werden soll, übergeben in der String-Darstellung
+	 * @return	Darstellung des Wertes im Zehnersystem als String-Darstellung
+	 * @throws 	NullPointerException	wenn der Parameter {@code wert} null ist.
+	 * @throws 	NumberFormatException	wenn der Parameter {@code wert} keinen umwandelbaren ganzzahligen Anteil hat.
+	 * @throws 	UnsupportedOperationException	wenn das erste Zeichen des Parameters {@code wert} ein '-' ist, 
+	 * 											da negative Zahlen hier nicht erlaubt sind
+	 */
+	public static String basisToDezString(int base, String value) throws NullPointerException, NumberFormatException, UnsupportedOperationException {
+		return basisToDezString(base, value, Zahl.SEP_ENG);
+	}
+	
+	/**
+	 * wandelt die übergebene Zahl zur spezifischen Basis in der String-Darstellung 
+	 * in eine Zahl zur Basis 10 und gibt diese als in der String-Darstellung zurück
+	 * @param base	die spezifische Basis des übergebenen wertes
+	 * @param value	der Zahlenwert, der umgewandelt werden soll, übergeben in der String-Darstellung
+	 * @param separator der spezifische Separator, mit dem der ganze und der Nachkommateil getrennt werden
+	 * @return	Darstellung des Wertes im Zehnersystem als String-Darstellung
+	 * @throws 	NullPointerException	wenn der Parameter {@code wert} null ist.
+	 * @throws 	NumberFormatException	wenn der Parameter {@code wert} keinen umwandelbaren ganzzahligen Anteil hat.
+	 * @throws 	UnsupportedOperationException	wenn das erste Zeichen des Parameters {@code wert} ein '-' ist, 
+	 * 											da negative Zahlen hier nicht erlaubt sind
+	 */
+	public static String basisToDezString(int base, String value, String separator) throws NullPointerException, NumberFormatException, UnsupportedOperationException {	
+		// Übergebene Zahl in ganzen Anteil und Nachkommastellen trennen
+		value = value.toUpperCase();
+		String ganzerWert = Zahl.separiereGanzenAnteil(base,value);
+		String nachKomma = Zahl.separiereNachkomma(base,value);
+		
+		// Strings die ganzen und Nachkommateil zu der übergebenen Basis 
+		// repräsentieren in double Zahlen zur Basis 10 umwandeln
+		double ganzDez = basisToDezGanz(base, ganzerWert);
+		double nachDez = basisToDezNachKomma(base, nachKomma);
+		
+		// Überprüfen ob es Nachkommastellen gibt
+		if(nachDez != 0.0) {
+			// Ja -> Rückgabe mit Nachkommateil
+			return String.valueOf((long)ganzDez) + separator + String.valueOf(nachDez).substring(2);
+		} else {
+			// Nein -> Rückgabe des ganzen Anteils
+			return String.valueOf((long)ganzDez);
+		}
+		
+	}
+
+
 
 
 }
