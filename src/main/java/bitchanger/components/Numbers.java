74ad2763c5fd1ccbe35bf2ce47962ead38a84808
Moritz Wolter <moritz.wolter@hs-bochum.de>
2020-05-23 13:22:40 +0200

    Erstellung der Klasse Numbers, zugehörig zu dem Interface ChangeableNumbers
    Klasse erstellt und implementierung des Interface ChangeableNumbers

---
+++ src/main/java/bitchanger/components/Numbers.java
@@ -0,0 +1,71 @@
+package bitchanger.components;
+
+public class Numbers implements ChangeableNumbers {
+
+	@Override
+	public void setHex(String hexValue) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setDec(String decValue) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setOct(String octValue) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setBin(String binValue) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setValue(String value, int baseOfValue) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setByteLength(int numberOfBytes) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String toHexString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toDecString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toOctString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toBinString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toBaseString(int base) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
