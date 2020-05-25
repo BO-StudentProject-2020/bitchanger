90204dca99be44f66e557f06211f8ac4d71f1404
Tim Muehle <tim.muehle@hs-bochum.de>
2020-05-25 18:39:21 +0200

    Build-Script Update
    Die Built-Skripte wurden so angepasst, dass die neue Programmversion nicht unnötig oft angepasst werden muss

--- build_installer_win.bat
+++ build_installer_win.bat
@@ -1,40 +1,41 @@
 @echo off
-rem Dieses Script erstellt mit Hilfe des jpackage Tools (OpenJDK14) aus einer ausfÅhrbaren JAR Datei
-rem einen nativen Installer fÅr Windows. Alle von dem Java Programm benîtigten Komponenten (JRE, javaFX, ...)
+rem Dieses Script erstellt mit Hilfe des jpackage Tools (OpenJDK14) aus einer ausfuehrbaren JAR Datei
+rem einen nativen Installer fuer Windows. Alle von dem Java Programm benoetigten Komponenten (JRE, javaFX, ...)
 rem werden dabei mit in den Installer eingebunden, sodass das Programm nach der Installation auf jedem Windows Betriebssystem
-rem unabhÑngig von anderen Programmen oder Installationen lauffÑhig ist.
+rem unabhaengig von anderen Programmen oder Installationen lauffaehig ist.
 
-rem Es werden zwei Installer in den gÑngigen Formaten bereitgestellt (.exe und .msi)
+rem Es werden zwei Installer in den gaengigen Formaten bereitgestellt (.exe und .msi)
 
-rem ---- Benîtigte Informationen in Variablen speichern ----------------------------------------------------------------
+rem ---- Benoetigte Informationen in Variablen speichern ----------------------------------------------------------------
 
-rem Mit den folgeneden Variablen kînnen die Grundlegenden Daten fÅr das Projekt eingestellt werden:
+rem Mit den folgeneden Variablen koennen die Grundlegenden Daten fuer das Projekt eingestellt werden:
 set NAME=Bitchanger
-set DESCRIPTION="Rechner fÅr beliebige Zahlensysteme"
-set VERSION=0.1.0
+set DESCRIPTION="Rechner fuer beliebige Zahlensysteme"
+if not defined VERSION set VERSION=0.1.0
 set VENDOR=Entwicklungsprojekt_EB2020
 rem set COPYRIGHT = ""
 rem set LICENSE_FILE = ""
 
 rem Einstellungen fuer jpackage:
 set MAIN_JAR=bitchanger-%VERSION%-jar-with-dependencies.jar
-set INPUT=target
-set OUT=installer
+if not defined INPUT set INPUT=installer\source
+if not defined OUT set OUT=installer\%VERSION%
 rem set ICON =
 
-rem Weitere Befehle fÅr jpackage:
-rem Installationspfad bei der Installation auswÑhlbar: --win-dir-chooser
+rem Weitere Befehle fuer jpackage:
+rem Installationspfad bei der Installation auswaehlbar: --win-dir-chooser
 rem Shortcut auf dem Desktop: --win-shortcut
-rem In das StartmenÅ aufnehmen: --win-menu
-rem StartmenÅgruppe aufwÑhlen: --win-menu-group <menu-group-name>
+rem In das Startmenue aufnehmen: --win-menu
+rem Startmenuegruppe aufwaehlen: --win-menu-group <menu-group-name>
 rem Installation auf Benutzerbasis: --win-per-user-install
-rem App Icon Ñndern: --icon <path/to/icon.ico>
+rem App Icon aendern: --icon <path/to/icon.ico>
 rem Linzenz Datei: --license-file <file>
+
 
 rem ---- Installer erzeugen ----------------------------------------------------------------------------------------------
 
 echo.
-echo Installer fÅr Windows werden erzeugt.
+echo Installer fuer Windows werden erzeugt.
 echo.
 
 rem ---- exe ----
@@ -82,7 +83,7 @@ jpackage ^
 echo.
 echo.
 
-rem ---- Auf BestÑtigung von Benutzer warten -----------------------------------------------------------------------------
-echo "Zum Abschlie·en eine beliebige Taste drÅcken"
+rem ---- Auf Bestaetigung von Benutzer warten -----------------------------------------------------------------------------
+echo "Zum Abschliessen eine beliebige Taste druecken"
 pause
 
