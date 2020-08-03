cc03b33bf3bbe2ce5c9d8bf26fd090eb2f7e0996
Moritz Wolter <moritz.wolter@hs-bochum.de>
2020-08-03 17:59:56 +0200

    (JavaDoc) EN - package-info

--- src/main/java/bitchanger/preferences/package-info.java
+++ src/main/java/bitchanger/preferences/package-info.java
@@ -4,4 +4,10 @@
  * Die Einstellungen betreffen die Optik und auch das Verhalten des Programms und können dauerhaft gespeichert werden, 
  * um das Programm an eigene Bedürfnisse anzupassen. Zudem sind die Standardeinstellungen wiederherstellbar.
  */
+
+/*	<!-- $LANGUAGE=EN -->
+ * With classes that are included in this package, components have access to global settings and can adjust these.
+ * These settings affect the optics but also the behavior of the program. All these parameters can be save for the optimal user experience. 
+ * Furthermore the default settings are recoverable at any time.
+ */
 package bitchanger.preferences;
