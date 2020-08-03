e4c1dc5c83010cfd4b730ff88d27780d7806e9d5
Moritz Wolter <moritz.wolter@hs-bochum.de>
2020-08-04 00:47:15 +0200

    (JavaDoc) EN - Viewable

--- src/main/java/bitchanger/gui/views/Viewable.java
+++ src/main/java/bitchanger/gui/views/Viewable.java
@@ -37,12 +37,42 @@ import javafx.scene.Scene;
  * @see Controller
  *
  */
+
+/*	<!-- $LANGUAGE=EN -->
+ * The interface {@code Viewable} defines the base for a view.
+ * <p>
+ * A view is representable in a JavaFX window (javafx.stage.Stage). The view encapsulate a {@code Scene},
+ * which can be represented in a stage. 
+ * </p>
+ * <p> <b> 
+ * The scene graph is constructed by a view, the view only sets the layout not the functions.
+ * </b> </p>
+ * <p> <b>
+ * A controller can be allocated to each view, to set functions to the operating elements in the scene graph.
+ * </b> </p>
+ * 
+ * @author Tim
+ * 
+ * @since Bitchanger 0.1.0
+ * @version 0.1.0
+ * 
+ * @see Controllable
+ * @see Controller
+ *
+ */
 public interface Viewable {
 	
 	/**	<!-- $LANGUAGE=DE -->
 	 * Gibt die repräsentierte {@code Scene} zurück
 	 * 
 	 * @return von der View repräsentierte {@code Scene}
+	 * 
+	 */
+	
+	/*	<!-- $LANGUAGE=EN -->
+	 * Returns the represented {@code Scene}
+	 * 
+	 * @return from the view represented {@code Scene}
 	 * 
 	 */
 	public Scene getScene();
@@ -53,12 +83,26 @@ public interface Viewable {
 	 * @return maximale Höhe der View
 	 * 
 	 */
+	
+	/*	<!-- $LANGUAGE=EN -->
+	 * Indicates the maximum height of the view
+	 * 
+	 * @return maximum height of the view
+	 * 
+	 */
 	public double getMaxHeigth();
 	
 	/**	<!-- $LANGUAGE=DE -->
 	 * Gibt die maximale Breite der View an
 	 * 
 	 * @return maximale Breite der View
+	 * 
+	 */
+	
+	/*	<!-- $LANGUAGE=EN -->
+	 * Indicates the maximum width of the view
+	 * 
+	 * @return maximum width of the view
 	 * 
 	 */
 	public double getMaxWidth();
@@ -69,12 +113,26 @@ public interface Viewable {
 	 * @return minimale Höhe der View
 	 * 
 	 */
+	
+	/*	<!-- $LANGUAGE=EN -->
+	 * Indicates the minimum height of the view
+	 * 
+	 * @return minimum height of the view
+	 * 
+	 */
 	public double getMinHeigth();
 	
 	/**	<!-- $LANGUAGE=DE -->
 	 * Gibt die minimale Breite der View an
 	 * 
 	 * @return minimale Breite der View
+	 * 
+	 */
+	
+	/*	<!-- $LANGUAGE=EN -->
+	 * Indicates the minimum width of the view
+	 * 
+	 * @return minimum width of the view
 	 * 
 	 */
 	public double getMinWidth();
