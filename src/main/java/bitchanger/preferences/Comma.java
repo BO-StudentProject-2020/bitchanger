9dbcd1babc88308d5b079009dadea42409c757e3
Moritz Wolter <moritz.wolter@hs-bochum.de>
2020-08-03 07:48:21 +0200

    (JavaDoc) EN - Comma

--- src/main/java/bitchanger/preferences/Comma.java
+++ src/main/java/bitchanger/preferences/Comma.java
@@ -9,7 +9,24 @@ package bitchanger.preferences;
  * @author Tim
  * 
  * @since Bitchanger 0.1.3
- * @version 0.1.3
+ * @version 0.1.4
+ * 
+ * @see Preferences#getCommaProperty()
+ * @see Preferences#getComma()
+ * @see Preferences#setComma(Comma)
+ *
+ */
+
+/*	<!-- $LANGUAGE=EN -->
+ * Enumeration of the wrapper class, that represents the character for a comma.
+ * <p>
+ * This class is used, so that no wrong character can be set as comma.
+ * </p>
+ * 
+ * @author Tim
+ * 
+ * @since Bitchanger 0.1.3
+ * @version 0.1.4
  * 
  * @see Preferences#getCommaProperty()
  * @see Preferences#getComma()
@@ -19,14 +36,22 @@ package bitchanger.preferences;
 public enum Comma {
 	
 	/** <!-- $LANGUAGE=DE --> Konstante, die das deutsche Komma (',') repräsentiert */
+	/* <!-- $LANGUAGE=EN --> Constant that represents the German comma (',') */
 	COMMA_DE(','),
 	
 	/** <!-- $LANGUAGE=DE --> Konstante, die das englische Komma ('.') repräsentiert */
+	/* <!-- $LANGUAGE=EN --> Constant that represents the English comma ('.') */
 	COMMA_EN('.');
 	
 	/**	<!-- $LANGUAGE=DE -->
 	 * 
 	 * Eingeschlossenes Komma-Zeichen
+	 * 
+	 */
+	
+	/*	<!-- $LANGUAGE=EN -->
+	 * 
+	 * Wrapped comma sign
 	 * 
 	 */
 	private char comma;
@@ -37,6 +62,13 @@ public enum Comma {
 	 * @param comma Kommazeichen
 	 * 
 	 */
+	
+	/*	<!-- $LANGUAGE=EN -->
+	 * Initialization of the comma sign in the constructor
+	 * 
+	 * @param comma comma sign
+	 * 
+	 */
 	private Comma(char comma) {
 		this.comma = comma;
 	}
@@ -45,6 +77,13 @@ public enum Comma {
 	 * Gibt das repräsentierte Komma-Zeichen zurück
 	 * 
 	 * @return das repräsentierte Komma-Zeichen zurück
+	 * 
+	 */
+	
+	/*	<!-- $LANGUAGE=EN -->
+	 * Returns the represented comma sign
+	 * 
+	 * @return the represented comma sign
 	 * 
 	 */
 	public char get() {
