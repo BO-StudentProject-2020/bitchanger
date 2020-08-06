d0938c4b501085bbf7d81d13c8f9d2a6e72d27a8
Tim Muehle <tim.muehle@hs-bochum.de>
2020-08-06 16:29:54 +0200

    (JavaDoc) ValueButton

--- src/main/java/bitchanger/gui/controls/ValueButton.java
+++ src/main/java/bitchanger/gui/controls/ValueButton.java
@@ -10,9 +10,7 @@
 
 package bitchanger.gui.controls;
 
-import bitchanger.preferences.Preferences;
 import bitchanger.calculations.ConvertingNumbers;
-import bitchanger.preferences.Comma;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.value.ChangeListener;
@@ -20,6 +18,10 @@ import javafx.beans.value.ObservableValue;
 import javafx.scene.Node;
 
 /**	<!-- $LANGUAGE=DE -->
+ * UnfocusedButton mit einer zusätzlichen Property für die Einstellung einer Basis.
+ * Wenn der Wert, den dieser Button repräsentiert, nicht zu der eingestellten Basis passt,
+ * deaktiviert sich dieser Button selbst. Ändert sich die Basis wieder so, dass der Wert
+ * wieder gültig ist, wird dieser Button automatisch aktiviert.
  * 
  * @author Tim Mühle
  * 
@@ -29,63 +31,76 @@ import javafx.scene.Node;
  */
 public class ValueButton extends UnfocusedButton{
 
-	//TODO Properties zum Ausblenden des Buttons bei unzulässigem Zeichen 	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!	!!
+	// Attribute	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##
+	/** <!-- $LANGUAGE=DE -->	Property, über das die Basis des aktuellen Zahlensystems eingestellt wird */
 	private IntegerProperty baseProperty;
 	
+	
+	// Konstruktoren	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##
+	/**	<!-- $LANGUAGE=DE -->
+	 * Erstellt einen ValueButton mit einem leeren String als Beschriftung
+	 */
 	public ValueButton() {
 		super();
 		init();
 	}
 
+	/**	<!-- $LANGUAGE=DE -->
+	 * Erstellt einen ValueButton mit einem spezifischen String und einem Icon als Beschriftung
+	 */
 	public ValueButton(String text, Node graphic) {
 		super(text, graphic);
 		init();
 	}
 
+	/**	<!-- $LANGUAGE=DE -->
+	 * Erstellt einen ValueButton mit einem spezifischen String als Beschriftung
+	 */
 	public ValueButton(String text) {
 		super(text);
 		init();
 	}
 	
 	
+	// Getter und Setter	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##
+	/**	<!-- $LANGUAGE=DE -->
+	 * Setzt den Wert für die Basis
+	 * 
+	 * @param base neue Basis
+	 */
 	public void setBase(int base) {
 		this.baseProperty.set(base);
 	}
 	
+	/**	<!-- $LANGUAGE=DE -->
+	 * Gibt den Wert das {@link #baseProperty} zurück
+	 * 
+	 * @return baseProperty dieses ValueButtons
+	 */
 	public IntegerProperty getBaseProperty() {
 		return this.baseProperty;
 	}
 	
+	
 	// Methoden	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##
+	/**	<!-- $LANGUAGE=DE -->
+	 * Initialisiert die {@link #baseProperty} und setzt Listener zum Aktivieren und Deaktivieren dieses Buttons
+	 * durch die eingestellte Basis
+	 * 
+	 * @see #setTextListener()
+	 * @see #setBaseListener()
+	 */
 	private void init() {
 		this.baseProperty = new SimpleIntegerProperty();
 		
 		setTextListener();
 		setBaseListener();
-		
-		
-		
-		Preferences.getCommaProperty().addListener(new ChangeListener<Comma>() {
-			@Override
-			public void changed(ObservableValue<? extends Comma> observable, Comma oldValue, Comma newValue) {
-				
-			}
-		});
-	}
-
-	private void setBaseListener() {
-		baseProperty.addListener(new ChangeListener<Number>() {
-			@Override
-			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-				if (ConvertingNumbers.isValueToBase(newValue.intValue(), getText())) {
-					setDisable(false);
-				} else {
-					setDisable(true);
-				}
-			}
-		});
 	}
 	
+	/**	<!-- $LANGUAGE=DE -->
+	 * Überprüft die textProperty und aktiviert bzw. deaktiviert diesen Button, wenn der neue Text
+	 * zur baseProperty passt bzw. nicht passt
+	 */
 	private void setTextListener() {
 		textProperty().addListener(new ChangeListener<String>() {
 			@Override
@@ -99,5 +114,24 @@ public class ValueButton extends UnfocusedButton{
 			}
 		});
 	}
+	
+	/**	<!-- $LANGUAGE=DE -->
+	 * Überprüft die baseProperty und aktiviert bzw. deaktiviert diesen Button, wenn der Text
+	 * zum neuen Wert der baseProperty passt bzw. nicht passt
+	 */
+	private void setBaseListener() {
+		baseProperty.addListener(new ChangeListener<Number>() {
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				if (ConvertingNumbers.isValueToBase(newValue.intValue(), getText())) {
+					setDisable(false);
+				} else {
+					setDisable(true);
+				}
+			}
+		});
+	}
+	
+	
 
 }
