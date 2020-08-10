a8390203b189e4da8efcdf358f442d26c924a7e6
Moritz Wolter <moritz.wolter@hs-bochum.de>
2020-08-10 16:56:25 +0200

    (JavaDoc) EN - package-info

--- src/main/java/bitchanger/gui/controls/package-info.java
+++ src/main/java/bitchanger/gui/controls/package-info.java
@@ -2,4 +2,8 @@
 /** <!-- $LANGUAGE=DE -->
  * Stellt angepasste Bedienelemente und Funktionen für die Benutzeroberfläche zur Verfügung
  */
+
+/* <!-- $LANGUAGE=EN -->
+ * Allocates adjusted operating elements and functions for the user interface
+ */
 package bitchanger.gui.controls;
