2e75e5def36a77dee4f11395a1efac3766584492
Moritz Wolter <moritz.wolter@hs-bochum.de>
2020-08-11 21:34:55 +0200

    (JavaDoc) EN - FXUtils

--- src/main/java/bitchanger/gui/controls/FXUtils.java
+++ src/main/java/bitchanger/gui/controls/FXUtils.java
@@ -16,6 +16,16 @@ import javafx.scene.layout.GridPane;
  * @version 0.1.4
  *
  */
+
+/* <!-- $LANGUAGE=EN -->
+ * Utility class to handle objects whose belong to the user interface.
+ * 
+ * @author Tim Mühle
+ * 
+ * @since Bitchanger 0.1.4
+ * @version 0.1.4
+ *
+ */
 public class FXUtils {
 
 	/** <!-- $LANGUAGE=DE -->
@@ -37,6 +47,29 @@ public class FXUtils {
 	 * @param columnCount	Anzahl der Spalten in der Tabelle
 	 * @param columnOffset	Verschiebung der ersten Spalte (wird nur in der ersten Zeile angewendet)
 	 * @param controls		Elemente in der Tabelle, mit der Reihenfolge von oben links nach unten rechts (Spalten werden vor den Zeilen inkrementiert)
+	 * 
+	 * @see #setGridConstraints(int, int, int, int, Queue, TriConsumer)
+	 */
+	
+	/* <!-- $LANGUAGE=EN -->
+	 * Sets the constrains of the nodes in a queue to position these in a GridPane.
+	 * <p>
+	 * The first nodes position is defined by the parameters and {@code firstColumn},
+	 * {@code firstRow} and {@code columnOffset}. The matrix gets spread of the amount of rows.
+	 * These rows are set by {@code columnCount} and get an automatic line break.
+	 * The constrains are set as long as there are elemts in the queue.
+	 * </p>
+	 * <p><b>
+	 * It is possible that the nodes take more place than just one column in the GridPane.
+	 * The column of the following node gets incremented accordingly to not overlay elements.
+	 * </b></p>
+	 * 
+	 * @param <T>			The type of the queue elements while runtime
+	 * @param firstColumn	First column of the resulting table
+	 * @param firstRow		First row of the resulting table
+	 * @param columnCount	Number of the columns in the table
+	 * @param columnOffset	Shift of the first column (only deployed in the first row)
+	 * @param controls		Elements in the table with the order from upper left to lower right (columns gets incremented before rows gets incremented)
 	 * 
 	 * @see #setGridConstraints(int, int, int, int, Queue, TriConsumer)
 	 */
@@ -63,6 +96,27 @@ public class FXUtils {
 	 * @param columnOffset	Verschiebung der ersten Spalte (wird nur in der ersten Zeile angewendet)
 	 * @param controls		Elemente in der Tabelle, mit der Reihenfolge von oben links nach unten rechts (Spalten werden vor den Zeilen inkrementiert)
 	 * @param consumer		TriConsumer, der die Daten verarbeitet
+	 */
+	
+	/* <!-- $LANGUAGE=EN -->
+	 * Proceed nodes as a table in a queue.
+	 * <p>
+	 * For each node inside the queue the column gets incremented. If the maximum count of columns reached,
+	 * the row gets incremented and the column get set on the start value.
+	 * The node is committed with the values of rows and columns to the TriConsumer to proceed these data.
+	 * </p>
+	 * <p><b>
+	 * It is possible that the nodes takes more than just one column in the GridPane.
+	 * The column of the following node gets incremented accordingly to not overlay elements.
+	 * </b></p>
+	 * 
+	 * @param <T>			The type of the queue elements while runtime
+	 * @param firstColumn	First column of the resulting table
+	 * @param firstRow		First row of the resulting table
+	 * @param columnCount	Number of the columns in the table
+	 * @param columnOffset	Shift of the first column (only deployed in the first row)
+	 * @param controls		Elements in the table with the order from upper left to lower right (columns gets incremented before rows gets incremented)
+	 * @param consumer		TriConsumer, that proceed the data
 	 */
 	public static <T extends Node > void setGridConstraints(int firstColumn, int firstRow, int columnCount, int columnOffset, Queue<T> controls, TriConsumer<Node, Integer, Integer> consumer) {
 		int row = firstRow;
@@ -95,6 +149,13 @@ public class FXUtils {
 	 * @param nodes		Sammlung aller anzupassenden Controls, die auch in Layout-Containern liegen dürfen
 	 * @param maxSize	Maximale Größe der Controls
 	 */
+	
+	/* <!-- $LANGUAGE=EN -->
+	 * Sets the maximum size of all controls inside an Iterable to the value of {@code maxSize}.
+	 * 
+	 * @param nodes		Collection of all controls that can be adjusted, and also are able to lie inside a layout container
+	 * @param maxSize	Maximum size of the controls
+	 */
 	public static void setMaxSizes(Iterable<Node> nodes, double maxSize) {
 		setMaxSizes(nodes, maxSize, maxSize);
 	}
@@ -106,6 +167,15 @@ public class FXUtils {
 	 * @param nodes		Sammlung aller anzupassenden Controls, die auch in Layout-Containern liegen dürfen
 	 * @param maxWidth	maximale Breite aller Controls
 	 * @param maxHeight	maximale Höhe aller Controls
+	 */
+	
+	/* <!-- $LANGUAGE=EN -->
+	 * Sets the maximum width of all controls inside an Iterable to the value of {@code maxWidth} and 
+	 * the maximum heights to the value of {@code maxHeight}.
+	 * 
+	 * @param nodes		Collection of all controls that can be adjusted, and also are able to lie inside a layout container
+	 * @param maxWidth	maximum width of all controls
+	 * @param maxHeight	maximum height of all controls
 	 */
 	public static void setMaxSizes(Iterable<Node> nodes, double maxWidth, double maxHeight) {
 		for (Node n : nodes) {
