8f23831c0073c839c9e5a250dd50fedcc5ae8c4c
Tim Muehle <tim.muehle@hs-bochum.de>
2020-10-08 13:51:01 +0200

    Update BitLength.java

--- src/main/java/bitchanger/calculations/BitLength.java
+++ src/main/java/bitchanger/calculations/BitLength.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2020 - Tim Muehle und Moritz Wolter
  * 
  * Entwicklungsprojekt im Auftrag von Professorin K. Brabender und Herrn A. Koch
- * Entwickelt für das AID-Labor der Hochschule Bochum
+ * Entwickelt fuer das AID-Labor der Hochschule Bochum
  * 
  */
 
@@ -10,16 +10,16 @@ package bitchanger.calculations;
 
 /**	<!-- $LANGUAGE=DE -->
  * 
- * @author Tim Mühle
+ * @author Tim M\u00FChle
  * 
  * @since Bitchanger 0.1.7
- * @version 0.1.7
+ * @version 0.1.8
  * 
  */
 // TODO JavaDoc
 public enum BitLength {
 	// TODO JavaDoc
-	_8_BIT("8-Bit", 8, 0xFF), _16_BIT("16-Bit", 16, 0xFFFF), _32_BIT("32-Bit", 32, 0xFFFFFFFFL), _64_BIT("64-Bit", 64, 0xFFFFFFFFFFFFFFFFl), UNKNOWN("Unbekannt", 0, 0x0);
+	_8_BIT("8-Bit", 8, 0xffL), _16_BIT("16-Bit", 16, 0xffffL), _32_BIT("32-Bit", 32, 0xffffffffL), _64_BIT("64-Bit", 64, 0xffffffffffffffffL), UNKNOWN("Unbekannt", 0, 0x0L);
 
 	private String description;
 	private int numberOfBits;
